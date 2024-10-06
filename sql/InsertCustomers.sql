a5e4d8d8f6a5dc9c608f497b7eeb270bc529e9dc
norrisbc1 <norrisbc1@gmail.com>
2024-10-06 14:54:28 -0400

    Add files via upload

---
+++ sql/InsertCustomers.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers (firstname, lastname, email, phone, address, company, notes)
+VALUES
+('Jim', 'Sodam', 'jim.sodam@goldman.com', '1-982-734-2655', 'Richmond, USA', 'Goldman Sachs', ''),
+('Carrie', 'Fisher', 'carrie.fisher@ms.com', '1-362-247-5498', 'Santa Fe, USA', 'Morgan Stanley', '');
